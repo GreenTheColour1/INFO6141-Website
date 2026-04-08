07f1d0e9b8bf87a8df538fd58f7ccebb9a2e7d7d
cam <dev@camerongreen.ca>
2026-04-08 19:51:49 +0000

    fixes

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,15 @@
 set -e
 
 echo "Building Nix image..."
-nix build .#blog-image
-
-IMAGE_PATH=$(nix build .#blog-image --print-out-path)/tarball/
+IMAGE_PATH=$(nix build .#blog-image --print-out-paths)
 echo "Loading Docker image from $IMAGE_PATH..."
-docker load < "$IMAGE_PATH"
+sudo docker load < "$IMAGE_PATH"
 
 echo "Tagging image as info6141-blog:latest..."
-docker tag info6141-blog:latest info6141-blog:latest 2>/dev/null || \
-  docker tag $(docker images 'info6141-blog:*' --format '{{.ID}}' | head -1) info6141-blog:latest
+sudo docker tag info6141-blog:latest info6141-blog:latest 2>/dev/null || \
+  sudo docker tag $(docker images 'info6141-blog:*' --format '{{.ID}}' | head -1) info6141-blog:latest
 
 echo "Starting containers with docker compose..."
-docker compose up -d
+sudo docker compose up -d
 
-echo "Done! App should be running at http://localhost:8042"
+echo "Done! App should be running at http://localhost:8043"
